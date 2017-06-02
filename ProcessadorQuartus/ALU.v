5760eb2f98446a4f427abd7fce8900d7a3d4eac1
AllenHichard <allenhichard@hotmail.com>
2017-06-02 13:47:04 -0300

    Alu + controle Alu

---
+++ ProcessadorQuartus/ALU.v
@@ -0,0 +1,55 @@
+module ALU(input[31:0] rs, rt, input[2:0] sinalOperacao,output[31:0] resultado, output zero, overflow);
+
+/* Ação da ALU | Entrada do Controle  
+	  and	    |			000 -
+	  or	    |			001 -
+     add     |       010 -
+ 	  mul 	 |			011 -
+	  div 	 |			100 -
+	   	    |			101 -
+	  sub     |			110 -
+	  slt	    |			111
+*/
+
+// eq $s, $t, offset;  se s = t salta, zero = 1, s != t continua zero = 0; calculo é realizado quando zero = 1
+
+reg [63:0] saida; // 32bits * 32bits = 64bits
+reg over, tzero;
+	always @ * 
+	case (sinalOperacao)
+	3'b000: // and
+		saida = rs & rt;
+	3'b001: // or
+		saida = rs | rt;
+	3'b010: // add
+		saida = rs + rt;	
+	3'b011: // mul
+		saida = rs * rt;
+	3'b100: // div
+		saida = rs / rt;
+	3'b110: // sub
+		saida = rs - rt;
+	3'b111: // slt
+		saida = (rs < rt); //Slt rd, rs, rt # if rs <rt, rd = 1; Else rt = 0
+	
+	endcase
+	
+	always @ ( * ) begin
+	
+		if(saida[63:32] > 32'b0)
+			over = 1'b1;
+		else
+			over = 1'b0;
+		if(saida == 1'b0)
+			tzero = 1'b1; // ativa a flag
+		else 
+			tzero = 1'b0;// permanece com o PC 
+			
+	end		
+		
+assign resultado = saida[31:0];
+assign overflow = over;	
+assign zero = tzero;
+
+endmodule
+	
---
+++ ProcessadorQuartus/controleALU.v
@@ -0,0 +1,38 @@
+module controleALU(	input[5:0] funct, input[1:0] opALU, output[2:0] sinalOperacao);
+
+/* opCode da Instrução | OpAlu | Operação    | Campo Funct | Ação da ALU | Entrada do Controle  
+	Tipo R 				  |   10  | and         |   100100	  |	  and	    |			000 -
+	Tipo R 				  |   10  | or          |   100101	  |	  or	    |			001 -
+	LW						  |   00  | load Word   |   xxxxxx 	  |     add     |       010 -
+	SW 					  |   00  |	Store Word  |   xxxxxx	  |     add     |			010 -
+	Tipo R 				  |   10  | add         |   100000	  |	  add	    |			010 -
+	Tipo R 				  |   10  | mul         |   000010    |	  mul 	 |			011 -
+	Tipo R 				  |   10  | div         |   011010    |	  div 	 |			100 -
+	Tipo R 				  |   10  |             |        	  |	   	    |			101 -
+	Branch equal		  |   01  |	Branch equal|   xxxxxx	  |     sub     |			110 -
+	Tipo R 				  |   10  | sub         |   100010	  |	  sub	    |			110 -
+	Tipo R 				  |   10  | slt         |   101010	  |	  slt	    |			111
+ */
+ 
+
+ 
+	reg [2:0] microSinal;
+	always @ ( * ) begin
+		if (funct == 6'b100100 && opALU == 2'b10)
+			microSinal <= 3'b000;
+		else if (funct == 6'b100101 && opALU == 2'b10)
+			microSinal <=3'b001;
+		else if ((opALU == 2'b00) || (funct == 6'b100000 && opALU == 2'b10))
+			microSinal <=3'b010;
+		else if (funct == 6'b000010 && opALU == 2'b10)
+			microSinal <=3'b011;
+		else if (funct == 6'b011010 && opALU == 2'b10)
+			microSinal <=3'b100;
+		else if ((opALU == 2'b01) || (funct == 6'b100010 && opALU == 2'b10))
+			microSinal <=3'b110;
+		else if (funct == 6'b101010 && opALU == 2'b10)
+			microSinal <=3'b111;
+	end
+	
+	assign sinalOperacao = microSinal;
+endmodule
